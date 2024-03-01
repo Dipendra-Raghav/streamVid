28664ed3a13b5fa2396a2fcd302f9a4087c0989a
Dipendra-Raghav <raghavdip19@gmail.com>
2024-03-01 11:37:05 +0530

    refresh and access token done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+import { 
+    registerUser,
+    loginUser, 
+    logoutUser,
+    refreshAccessToken
+} from "../controllers/user.controller.js";
 
 import {upload} from "../middleware/multer.middleware.js"
 
@@ -18,4 +25,8 @@ router.route("/register").post(
 
     ]),
     registerUser)
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+//
 export default router
